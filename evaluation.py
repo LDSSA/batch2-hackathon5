c7558a00c3b3cdf96a3c4d571d1f0433f14b2144
Sam Hopkins <sam@daredata.engineering>
2018-08-25 16:21:48 +0100

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -15,7 +15,7 @@ def read_file(filename, sep):
 
 
 def evaluate():
-    test_songs = read_file('test_user_played_songs.txt', ' ')
+    test_songs = read_file('y_true.txt', ' ')
     pred_songs = read_file('results.csv', ',')
 
     actual = []
